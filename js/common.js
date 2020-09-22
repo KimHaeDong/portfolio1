0569872a84af26595aedca34ca205a8eea62144c
KimHaeDong <68522342+KimHaeDong@users.noreply.github.com>
2020-09-22 15:04:55 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -27,6 +27,27 @@
         $('#kimBox').load(url)
     })
 
+    $(window).scroll(function(){
+        var sct = $(this).scrollTop()
+        var actNear = $('.company').offset().top - $(this).height()/2
+        if (sct >= actNear) {
+            $('.company .container').addClass('on')
+        } else if (sct===0){
+            $('.company .container').removeClass('on')
+        }
+
+    })
+
+    $(window).scroll(function(){
+        var sct = $(this).scrollTop()
+        var actNear = $('.studio').offset().top - $(this).height()/2
+        if (sct >= actNear) {
+            $('.studio .studioInner').addClass('on')
+        } else if (sct===0){
+            $('.studio .studioInner').removeClass('on')
+        }
+
+    })
 
 
 })(jQuery)
