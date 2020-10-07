eeea5ce338402c2b667a1d698988c69241180888
KimHaeDong <68522342+KimHaeDong@users.noreply.github.com>
2020-10-07 16:33:55 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,26 +2,62 @@
 
 
  init()
- var flag = true;
+ var init = true;
  function init() {
    var ww = $(window).width()
-   if ( ww>767 && flag ) {
-      $('.h1Nav .nav').show()
-      $('.depth1 > li').removeClass('on')
-      $('.open_nav, .close_nav, .depth2').hide()
+   if ( ww>767 ) {
       $('html').addClass('pc').removeClass('mobile')
-      flag = false
-   } else if ( ww<=767 && !flag ) {
-      $('.open_nav').show()
-      $('.h1Nav .nav, .depth2').hide()
+      if ( init ) {
+        $('.h1Nav .nav').show()
+        $('.depth1 > li').removeClass('on')
+        $('.open_nav, .close_nav, .depth2').hide()
+        init = false
+      }      
+   } else if ( ww<=767 ) {      
       $('html').addClass('mobile').removeClass('pc')
-      flag = true
+      if ( !init ) {
+        $('.open_nav').show()
+        $('.h1Nav .nav, .depth2').hide()
+        init = true
+      }
    }
  }
 
- $(window).on('resize', function(){
-   init()
- })
+    $(window).on('resize', function(){
+        init()
+    })
+
+    // 모바일화면에서 1단계메뉴 클릭했을때 2단계메뉴 보이게 하기
+  $(".depth1 > li").on('click', function(){
+      if ( $('html').hasClass('mobile') ) {
+        $(this).toggleClass('on')
+        $(this).find('.depth2').stop().slideToggle(300)
+        $(this).siblings().each(function(){
+            if (  $(this).css('display') === 'block' ) {
+              $(this).find('.depth2').slideUp(300)
+              $(this).removeClass('on')
+            }
+        })
+      }
+  })
+
+  // pc화면에서 1단계메뉴에 호버했을때 2단계메뉴 보이게 하기
+  $('.depth1 > li').hover(
+    function(){
+      if ( $('html').hasClass('pc') ) {
+        $(this).find('.depth2').stop().slideDown(300)
+      }
+    },
+    function(){
+      if ( $('html').hasClass('pc') ) {
+        $(this).find('.depth2').stop().slideUp(300)
+      }
+    }
+  )
+
+
+
+    // 링크연결
 
     $(window).load(function(){
         $('.introAni').delay(500).fadeOut(500)
@@ -50,6 +86,9 @@
         $('#kimBox').load(url)
     })
 
+
+    
+
     $(window).scroll(function(){
         var sct = $(this).scrollTop()
         var actNear = $('.company').offset().top - $(this).height()/2
@@ -72,19 +111,19 @@
 
     })
 
-
+    // 햄버거버튼 클릭시 네비박스 나타나기
     $('.h1Nav .open_nav').on('click', function(){
         $(this).next().stop().slideDown(300)
         $(this).hide()
         $(this).nextAll('.close_nav').css({display:'block'})
-      })
+    })
     
       // 닫기버튼 클릭시 네비박스 사라지기
-      $('.h1Nav .close_nav').on('click', function(){
-        $(this).prev().stop().slideUp(300)
-        $(this).hide()
-        $(this).prevAll('.open_nav').css({display:'block'})
-        $('.depth2').hide()
-        $('.h1Nav .nav .depth1 > li').removeClass('on')
-      })
+    $('.h1Nav .close_nav').on('click', function(){
+       $(this).prev().stop().slideUp(300)
+       $(this).hide()
+       $(this).prevAll('.open_nav').css({display:'block'})
+       $('.depth2').hide()
+       $('.h1Nav .nav .depth1 > li').removeClass('on')
+    })
 })(jQuery)
